91897837972b2aa02e20310a49ef4a9f1fc19757
williamsteffens <william.f.f.steffens@gmail.com>
2022-09-23 15:00:49 +0200

    works now B)

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 /*
 Cell counting program - Ass1
 */
+#define DEBUG 1
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h> 
+#include <stdbool.h>
 #include <string.h>
 
 #include "cbmp.h"
@@ -13,47 +14,13 @@ Cell counting program - Ass1
 // TODO: add enum for drawing:)
 
 unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-unsigned char intermedia_image0[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-unsigned char intermedia_image1[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-unsigned char (*image0_ptr)[BMP_HEIGTH][BMP_CHANNELS] = intermedia_image0;
-unsigned char (*image1_ptr)[BMP_HEIGTH][BMP_CHANNELS] = intermedia_image1;
-unsigned char (*tmp_ptr)[BMP_HEIGTH][BMP_CHANNELS];
-bool debug = false; 
+
 int step = 0;
 int cellCount = 0; 
 char buffer[50];
 char output_file_name[50];
 
 
-
-void create_binary(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH]) {
-  for (int x = 0; x < BMP_WIDTH; ++x) {
-    for (int y = 0; y < BMP_HEIGTH; ++y) {
-        // TODO: otsu metode???
-        int avg = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) / 3;
-        output_image[x][y] = avg > 90 ? 255 : 0;
-    }
-  }
-}
-
-void create_binary_image(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]) {
-  for (int x = 0; x < BMP_WIDTH; ++x) {
-    for (int y = 0; y < BMP_HEIGTH; ++y) {
-      for (int c = 0; c < BMP_CHANNELS; ++c) {
-        int avg = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) / 3;
-        output_image[x][y][c] = avg > 90 ? 255 : 0;
-      }
-    }
-  }
-}
-
-// void erode(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH]) {
-//   for (int x = 0; x < BMP_WIDTH; x++) {
-//     for (int y = 0; y < BMP_HEIGTH; y++) {
-//       // do stuff here to erode the array :)
-//     }
-//   }
-// }
 
 bool is_on_edge(int x, int y, int width, int height) {
   bool max = x >= width - 1 || y >= height - 1;
@@ -65,83 +32,20 @@ bool is_on_edge(int x, int y, int width, int height) {
     return false; 
 }
 
-bool erode_image(unsigned char in_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char out_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]) {
-  unsigned char structing_element[3][3] = {{0,1,0},{1,1,1},{0,1,0}};
-  bool wasEroded = false;
-  bool erodePixel;   
-
-  for (int x = 0; x < BMP_WIDTH; ++x) {
-    for (int y = 0; y < BMP_HEIGTH; ++y) {
-      if (is_on_edge(x, y, BMP_WIDTH, BMP_HEIGTH)) {
-        if (in_image[x][y][0] == 255) {
-          out_image[x][y][0] = 0;
-          out_image[x][y][1] = 0;
-          out_image[x][y][2] = 0;
-          wasEroded = true;
-        }
-      } else {
-        erodePixel = false; 
-        for (int i = 0; i < 3; ++i) {
-          for (int j = 0; j < 3; ++j) {
-            // no need to check all of the indexes, so break as needed
-            if (structing_element[i][j] == 1 && in_image[x + i - 1][y + j - 1][0] == 0) {
-              erodePixel = true; 
-              break;
-            }
-          }
-          if (erodePixel) {
-            out_image[x][y][0] = 0;
-            out_image[x][y][1] = 0;
-            out_image[x][y][2] = 0; 
-            break;
-          }
-        }
-
-        if (!erodePixel) {
-          out_image[x][y][0] = 255;
-          out_image[x][y][1] = 255;
-          out_image[x][y][2] = 255;
-        }
-      }
-    }
-  }
-
-  return wasEroded; 
-}
-
-bool detection_frame_clear(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int x, int y, int totalWidth) {
-  for (int i = 0; i < totalWidth; ++i)
-    for (int j = 0; j < totalWidth; ++j)
-      if (i == 0 || i == totalWidth - 1 || j == 0 || j == totalWidth - 1)
-        if (image[x + i][y + j][0] == 255)
-          return false;
-
-  return true; 
-}
-
-bool cell_detected(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int x, int y, int capAreaWidth) {
-  for (int i = 1; i <= capAreaWidth; ++i)
-    for (int j = 1; j <= capAreaWidth; ++j)
-      if (image[x + i][y + j][0] == 255)
-        return true;
-
-  return false; 
-}
-
 void draw_detection_indication(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int x, int y, int totalWidth) {
-  int detection_indication[13][13] = {{0,0,1,0,0,0,0,1,0,0,0,0,0},
-                                      {0,0,1,1,0,0,0,0,1,0,0,0,0},
+  int detection_indication[13][13] = {{0,0,1,0,0,0,0,0,0,0,0,0,0},
+                                      {0,0,1,1,0,0,0,1,1,1,0,0,0},
                                       {0,0,1,1,1,0,0,0,0,1,0,0,0},
-                                      {0,0,1,1,1,0,0,0,0,0,1,0,0},
-                                      {0,0,1,1,0,0,0,0,0,0,1,0,0},
-                                      {0,0,1,0,0,0,0,0,0,0,1,0,0},
-                                      {0,0,1,0,0,0,0,0,0,0,1,0,0},
-                                      {0,0,1,0,0,0,0,0,0,0,1,0,0},
-                                      {0,0,1,1,0,0,0,0,0,0,1,0,0},
-                                      {0,0,1,1,1,0,0,0,0,0,1,0,0},
                                       {0,0,1,1,1,0,0,0,0,1,0,0,0},
-                                      {0,0,1,1,0,0,0,0,1,0,0,0,0},
-                                      {0,0,1,0,0,0,0,1,0,0,0,0,0},}; 
+                                      {0,0,1,1,0,0,0,0,0,1,0,0,0},
+                                      {0,0,1,0,0,0,0,0,0,1,0,0,0},
+                                      {0,0,1,1,1,1,1,1,0,1,0,0,0},
+                                      {0,0,1,0,0,0,0,1,0,1,0,0,0},
+                                      {0,0,1,1,0,0,0,1,0,1,0,0,0},
+                                      {0,0,1,1,1,0,0,0,0,1,0,0,0},
+                                      {0,0,1,1,1,0,0,0,0,1,0,0,0},
+                                      {0,0,1,1,0,0,0,1,1,1,0,0,0},
+                                      {0,0,1,0,0,0,0,0,0,0,0,0,0},}; 
   
   for (int i = 0; i < totalWidth; ++i)
     for (int j = 0; j < totalWidth; ++j)
@@ -166,7 +70,98 @@ void draw_detection_indication(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CH
   //     }
 }
 
-void remove_cell(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int x, int y, int capAreaWidth) {
+
+// preprocess
+#if DEBUG
+
+
+
+unsigned char intermedia_image0[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char intermedia_image1[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char (*image0_ptr)[BMP_HEIGTH][BMP_CHANNELS] = intermedia_image0;
+unsigned char (*image1_ptr)[BMP_HEIGTH][BMP_CHANNELS] = intermedia_image1;
+unsigned char (*tmp_ptr)[BMP_HEIGTH][BMP_CHANNELS];
+
+void create_binary_image(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]) {
+  for (int x = 0; x < BMP_WIDTH; ++x) {
+    for (int y = 0; y < BMP_HEIGTH; ++y) {
+      int avg = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]);
+      for (int c = 0; c < BMP_CHANNELS; ++c) {
+        output_image[x][y][c] = avg > 270 ? 255 : 0;
+      }
+    }
+  }
+}
+
+bool erode_image(unsigned char in_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char out_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]) {
+  unsigned char structing_element[3][3] = {{0,1,0},{1,1,1},{0,1,0}};
+  bool wasEroded = false;
+  bool erodePixel;
+
+  for (int x = 0; x < BMP_WIDTH; ++x) {
+    for (int y = 0; y < BMP_HEIGTH; ++y) {
+
+      // Should only erode if the pixel is white
+      if (in_image[x][y][0] == 0) {
+        out_image[x][y][0] = in_image[x][y][0];
+        out_image[x][y][1] = in_image[x][y][1];
+        out_image[x][y][2] = in_image[x][y][2];
+        continue;
+      }
+
+      erodePixel = false;
+      if (is_on_edge(x, y, BMP_WIDTH, BMP_HEIGTH)) {
+        if (in_image[x][y][0] == 255)
+          erodePixel = true;
+      } else {
+        for (int i = 0; i < 3; ++i) {
+          for (int j = 0; j < 3; ++j)
+            if (structing_element[i][j] == 1 && in_image[x + i - 1][y + j - 1][0] == 0) {
+              erodePixel = true;
+              // TODO: consider using <goto> here so you don't need to break twice
+              break;
+            }
+          if (erodePixel)
+            break;
+        }
+      }
+
+      if (erodePixel) {
+        out_image[x][y][0] = 0;
+        out_image[x][y][1] = 0;
+        out_image[x][y][2] = 0;
+        wasEroded = true;
+      } else {
+        out_image[x][y][0] = in_image[x][y][0];
+        out_image[x][y][1] = in_image[x][y][1];
+        out_image[x][y][2] = in_image[x][y][2];
+      }
+    }
+  }
+
+  return wasEroded; 
+}
+
+bool detection_frame_clear_image(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int x, int y, int totalWidth) {
+  for (int i = 0; i < totalWidth; ++i)
+    for (int j = 0; j < totalWidth; ++j)
+      if (i == 0 || i == totalWidth - 1 || j == 0 || j == totalWidth - 1)
+        if (image[x + i][y + j][0] == 255)
+          return false;
+
+  return true; 
+}
+
+bool cell_detected_image(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int x, int y, int capAreaWidth) {
+  for (int i = 1; i <= capAreaWidth; ++i)
+    for (int j = 1; j <= capAreaWidth; ++j)
+      if (image[x + i][y + j][0] == 255)
+        return true;
+
+  return false; 
+}
+
+void remove_cell_image(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int x, int y, int capAreaWidth) {
   // TODO: maybe only color white parts of the image; there's no need to color black pixels black
   for (int i = 1; i <= capAreaWidth; ++i)
     for (int j = 1; j <= capAreaWidth; ++j) {
@@ -185,16 +180,43 @@ void detect_cells(unsigned char in_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], u
   for (int x = 0; x < BMP_WIDTH - capAreaWidth; ++x)
     for (int y = 0; y < BMP_WIDTH - capAreaWidth; ++y)
       
-      if (detection_frame_clear(in_image, x, y, capAreaWidth + frameWidth))
-        if (cell_detected(in_image, x, y, capAreaWidth)) {
+      if (detection_frame_clear_image(in_image, x, y, capAreaWidth + frameWidth))
+        if (cell_detected_image(in_image, x, y, capAreaWidth)) {
           draw_detection_indication(out_image, x, y, capAreaWidth + frameWidth);
-          remove_cell(in_image, x, y, capAreaWidth);
+          remove_cell_image(in_image, x, y, capAreaWidth);
           ++*cellCount;
           if (printCoords)
             // TODO: modify this to be the center
             printf("\tcell #%d: (%d, %d)\n", *cellCount, x, y);
         }
 }
+
+
+
+#else
+
+
+
+unsigned char intermedia_image0[BMP_WIDTH][BMP_HEIGTH];
+unsigned char intermedia_image1[BMP_WIDTH][BMP_HEIGTH];
+unsigned char (*image0_ptr)[BMP_HEIGTH] = intermedia_image0;
+unsigned char (*image1_ptr)[BMP_HEIGTH] = intermedia_image1;
+unsigned char (*tmp_ptr)[BMP_HEIGTH];
+
+void create_binary(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH]) {
+  for (int x = 0; x < BMP_WIDTH; ++x) {
+      for (int y = 0; y < BMP_HEIGTH; ++y) {
+          // TODO: otsu metode???
+          int avg = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]);
+          output_image[x][y] = avg > 270 ? 255 : 0;
+      }
+  } 
+    
+}
+
+
+
+#endif
 
 
 
@@ -218,88 +240,24 @@ int main(int argc, char** argv) {
   printf("Cell detection results:\n");
 
 
-
-
-
-
-
-
-
-
-
-
-  // erode_image(intermedia_image0, intermedia_image1);
-  // erode_image(intermedia_image1, intermedia_image0);
-  // erode_image(intermedia_image0, intermedia_image1);
-  // erode_image(intermedia_image1, intermedia_image0);
-  // erode_image(intermedia_image0, intermedia_image1);
-  // erode_image(intermedia_image1, intermedia_image0);
-  // erode_image(intermedia_image0, intermedia_image1);
-  // erode_image(intermedia_image1, intermedia_image0);
-  // erode_image(intermedia_image0, intermedia_image1);
-  // erode_image(intermedia_image1, intermedia_image0);
-  // printf("%d", erode_image(intermedia_image0, intermedia_image1));
-
-  // detect_cells(intermedia_image1, input_image, &cellCount, false);
-
-  // write_bitmap(intermedia_image1, argv[2]);
-
-
-  // NOW WITH POINTERS - IT now doesn't work
-  // erode_image(image0_ptr, image1_ptr);
-  // erode_image(image1_ptr, image0_ptr);
-  // printf("%d", erode_image(image0_ptr, image1_ptr));
-
-  // tmp_ptr = image0_ptr;
-  // image0_ptr = image1_ptr;
-  // image1_ptr = tmp_ptr;
-
-
-
-  // detect_cells(image1_ptr, input_image, &cellCount, false);
-
-  // write_bitmap(image1_ptr, argv[2]);
-
-  // return 0; 
-
-
-
-
-
-
-
-
   // TODO: MAKE THE DETECITON FRAME TRUE 
-  // TODO: MAKE ERODE RETURN TRUE!
-  // TODO: MAKE ERODE NOT DO WEIRD STUFF
 
 
-
-
-
-
-
-  while(1) {
-    erode_image(image0_ptr, image1_ptr);
-
+  while(erode_image(image0_ptr, image1_ptr)) {
     // print steps for debugging
-    if (debug) {
+    if (DEBUG) {
       memcpy(output_file_name, argv[2] + 2, strlen(argv[2]));
       snprintf(buffer, sizeof buffer, "./step_%d_%s", step++, output_file_name);
       write_bitmap(image1_ptr, buffer);
     }
-    step++;
 
     // Step 5 and 6: Detect cells and generate output image
-    detect_cells(image1_ptr, input_image, &cellCount, true);
+    detect_cells(image1_ptr, input_image, &cellCount, false);
 
     // Swap ptr for the intermedia images
     tmp_ptr = image0_ptr;
     image0_ptr = image1_ptr;
     image1_ptr= tmp_ptr;
-
-    if (step > 25)
-      break;
   }
 
   printf("\tA total of %d %s detected.\n", cellCount, cellCount == 1 ? "cell was" : "cells were");
