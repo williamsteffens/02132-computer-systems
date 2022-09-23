bae26d7e5550c95dd9f1f14e839f5853c2a6189e
williamsteffens <william.f.f.steffens@gmail.com>
2022-09-23 07:50:09 +0200

    almost works

--- src/main.c
+++ src/main.c
@@ -13,8 +13,15 @@ Cell counting program - Ass1
 // TODO: add enum for drawing:)
 
 unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-unsigned char intermedia_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char intermedia_image0[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char intermedia_image1[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char (*image0_ptr)[BMP_HEIGTH][BMP_CHANNELS] = intermedia_image0;
+unsigned char (*image1_ptr)[BMP_HEIGTH][BMP_CHANNELS] = intermedia_image1;
+unsigned char (*tmp_ptr)[BMP_HEIGTH][BMP_CHANNELS];
+bool debug = true; 
+int step = 0;
 int cellCount = 0; 
+char buffer[50];
 
 
 
@@ -57,7 +64,7 @@ bool is_on_edge(int x, int y, int width, int height) {
     return false; 
 }
 
-bool erode_image(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]) {
+bool erode_image(unsigned char in_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char out_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]) {
   unsigned char structing_element[3][3] = {{0,1,0},{1,1,1},{0,1,0}};
   bool wasEroded = false;
   bool erodePixel;   
@@ -65,10 +72,10 @@ bool erode_image(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS],
   for (int x = 0; x < BMP_WIDTH; ++x) {
     for (int y = 0; y < BMP_HEIGTH; ++y) {
       if (is_on_edge(x, y, BMP_WIDTH, BMP_HEIGTH)) {
-        if (input_image[x][y][0] == 255) {
-          output_image[x][y][0] = 0;
-          output_image[x][y][1] = 0;
-          output_image[x][y][2] = 0;
+        if (in_image[x][y][0] == 255) {
+          out_image[x][y][0] = 0;
+          out_image[x][y][1] = 0;
+          out_image[x][y][2] = 0;
           wasEroded = true;
         }
       } else {
@@ -76,23 +83,23 @@ bool erode_image(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS],
         for (int i = 0; i < 3; ++i) {
           for (int j = 0; j < 3; ++j) {
             // no need to check all of the indexes, so break as needed
-            if (structing_element[i][j] == 1 && input_image[x + i - 1][y + j - 1][0] == 0) {
+            if (structing_element[i][j] == 1 && in_image[x + i - 1][y + j - 1][0] == 0) {
               erodePixel = true; 
               break;
             }
           }
           if (erodePixel) {
-            output_image[x][y][0] = 0;
-            output_image[x][y][1] = 0;
-            output_image[x][y][2] = 0; 
+            out_image[x][y][0] = 0;
+            out_image[x][y][1] = 0;
+            out_image[x][y][2] = 0; 
             break;
           }
         }
 
         if (!erodePixel) {
-          output_image[x][y][0] = 255;
-          output_image[x][y][1] = 255;
-          output_image[x][y][2] = 255;
+          out_image[x][y][0] = 255;
+          out_image[x][y][1] = 255;
+          out_image[x][y][2] = 255;
         }
       }
     }
@@ -121,9 +128,9 @@ bool cell_detected(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int
 }
 
 void draw_detection_indication(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], int x, int y, int totalWidth) {
-  int detection_indication[13][13] = {{0,0,0,0,0,0,0,0,0,0,0,0,0},
-                                      {0,0,0,0,0,0,0,0,0,0,0,0,0},
-                                      {0,0,0,0,0,0,0,0,0,0,0,0,0},
+  int detection_indication[13][13] = {{0,0,0,0,0,1,0,0,0,0,0,0,0},
+                                      {0,0,0,0,0,1,0,0,0,0,0,0,0},
+                                      {0,0,0,0,0,1,0,0,0,0,0,0,0},
                                       {0,0,0,0,0,1,0,0,0,0,0,0,0},
                                       {0,0,0,0,0,1,0,0,0,0,0,0,0},
                                       {0,0,0,0,0,1,0,0,0,0,0,0,0},
@@ -142,6 +149,12 @@ void draw_detection_indication(unsigned char image[BMP_WIDTH][BMP_HEIGTH][BMP_CH
         image[x + i][y + j][1] = 0;
         image[x + i][y + j][2] = 255;
       }
+
+  // do option for getting the silhoutte of the cell 
+  // curly brackets
+  // washing machine symbol 
+  // Lorem Ipsum 
+  // [] or only corners
 
   // for (int i = 0; i < totalWidth; ++i)
   //   for (int j = 0; j < totalWidth; ++j)
@@ -185,6 +198,7 @@ void detect_cells(unsigned char in_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], u
 
 
 int main(int argc, char** argv) {
+
   if (argc != 3) {
       fprintf(stderr, "Usage: %s <input file path> <output file path>\n", argv[0]);
       exit(1);
@@ -196,27 +210,31 @@ int main(int argc, char** argv) {
   read_bitmap(argv[1], input_image);
   
   // Step 2 and 3: Convert from RGB to GrayScale and apply the binary threshold to create a binary image
-  create_binary_image(input_image, intermedia_image);
+  create_binary_image(input_image, image0_ptr);
 
   // TODO: Why erode the image first? Shouldn't we detect the cells we can and then erode? or are we trying to get rid of noise? 
   // Step 4: Erode the binary image
-  printf("Cell detection results:");
+  printf("Cell detection results:\n");
 
-  int step = 0;
-  while(erode(intermedia_image)) {
+  while(erode_image(image0_ptr, image1_ptr)) {
     // print steps for debugging
-    write_bitmap(intermedia_image, "step_%d_%s", step++, argv[2]);
+    snprintf(buffer, sizeof buffer, "step_%d_%s", step++, argv[2]);
+    write_bitmap(image1_ptr, buffer);
 
-    // Step 5 and 6: Detect cells and print results
-    detect_cells(intermedia_image, input_image, &cellCount, true);
+    // Step 5 and 6: Detect cells and generate output image
+    detect_cells(image1_ptr, input_image, &cellCount, true);
+
+    // Swap ptr for the intermedia images
+    tmp_ptr = image0_ptr;
+    image0_ptr = image1_ptr;
+    image1_ptr= tmp_ptr;
   }
 
-  printf("\tA total of %d %s detected", cellCount, cellCount > 1 ? "cells were" : "cell was");
+  printf("\tA total of %d %s detected.\n", cellCount, cellCount == 1 ? "cell was" : "cells were");
 
   // Step 7: Save output image and print results
   write_bitmap(input_image, argv[2]);
 
-  printf("cells detected: %d\n", cellCount);
   printf("Done!\n");
   return 0;
 }
